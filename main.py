180409c0117359428f79661a669bc863a9b36a9b
Siang Meng <siang-meng@live.com>
2025-04-15 11:03:09 +0800

    Change from Ollama to OpenAI

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import asyncio
 import time
+import os
 
 from langchain_ollama.llms import OllamaLLM
+from langchain.chat_models import ChatOpenAI 
+from dotenv import load_dotenv
+
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.schema import BaseOutputParser
 
@@ -12,7 +16,11 @@ class SimpleOutputParser(BaseOutputParser):
     def parse(self, text: str) -> str:
         return text.strip()
 
-model = OllamaLLM(model="llama3.2:1b",keep_alive=True,top_k=1)
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+# model = OllamaLLM(model="llama3.2:1b",keep_alive=True,top_k=1)
+model = ChatOpenAI(openai_api_key = api_key)
 
 # Async function to process a single file
 def process_file(file_path: str, code: str, extension: str):
